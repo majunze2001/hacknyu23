960671908f3ce63053394c972c1a4ad8df9183c3
Jeff Ma <jm9527@nyu.edu>
2023-02-19 11:10:47 -0500

    bug fixed

--- app.js
+++ app.js
@@ -53,12 +53,12 @@ setInterval(function () {
     if (!io.sockets.sockets.size) {
         return;
     }
-    if (data.getGlobalCarbon()>=600){
-        data.reset();
-    }
     data.addCarbon(-0.05);
     io.emit('carbon', {
         newData: data.getLastCarbon(),
         carbonFactor: carbonFactor(data.getGlobalCarbon())
     })
+    if (data.getGlobalCarbon()>=600){
+        data.reset();
+    }
 }, 3000);
